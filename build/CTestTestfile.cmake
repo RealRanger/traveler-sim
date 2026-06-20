b30424e934edbf8dfa6148827ceddc97d1151e59
RealRangerC123456 <realrangerc123456@gmail.com>
2026-06-20 15:15:38 +0200

    Add rocket config file class

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/Software/Simulations/Aerospace/traveler-sim
-# Build directory: C:/Dev/Software/Simulations/Aerospace/traveler-sim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
